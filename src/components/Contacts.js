4f8e559abeb72b6e678fa634370bc03d3a150ce5
helene-picard <31765629+helene-picard@users.noreply.github.com>
2019-01-31 13:51:00 +0100

    contact manager with context api
    handle state and function with context api
    user consumer and provider to pass on state
    use dispatch function to delete contact

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,50 +1,22 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@test.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Jane Doe",
-        email: "janedoe@test.com",
-        phone: "559-595-5555"
-      },
-      {
-        id: 3,
-        name: "Jean Doe",
-        email: "jeandoe@test.com",
-        phone: "525-555-5555"
-      }
-    ]
-  };
-
-  deleteContact = id => {
-    const { contacts } = this.state;
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    this.setState({
-      contacts: newContacts
-    });
-  };
-
   render() {
-    const { contacts } = this.state;
     return (
-      <div>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </div>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
